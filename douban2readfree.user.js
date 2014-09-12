f0ea87f65872fb1285cf428b4672b959ab4d6c21
Wang Jiajun <Amesists@gmail.com>
2014-09-12 10:57:16 +0800

    Version bump to 1.0.1
    Seperate css file.
    Use HEAD instead of GET.

--- douban2readfree.user.js
+++ douban2readfree.user.js
@@ -4,9 +4,11 @@
 // @description 在“豆瓣读书”页面增加到ReadFree电子书的传送门
 // @icon        https://raw.githubusercontent.com/JiajunW/douban2readfree/master/res/icon.png
 // @include     http://book.douban.com/subject/*
-// @version     1.0.0
+// @version     1.0.1
+// @resource    custom_css https://raw.githubusercontent.com/JiajunW/douban2readfree/master/style/style.css
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
+// @grant       GM_getResourceText
 // ==/UserScript==
 
 (function() {
@@ -20,23 +22,7 @@
     }
 
     function add_readfree_style() {
-        var custom_css = "\
-          #readfree-link {\
-            position: fixed;\
-            top: 160px;\
-            left: -10px;\
-            padding: 10px 20px 10px 30px;\
-            border-radius: 10px;\
-            background: -webkit-linear-gradient(top, rgba(50, 74, 105, 0.8), rgba(50, 74, 105, 1));\
-            background:   -mozc-linear-gradient(top, rgba(50, 74, 105, 0.8), rgba(50, 74, 105, 1));\
-            background:         linear-gradient(to bottom, rgba(50, 74, 105, 0.8), rgba(50, 74, 105, 1));\
-          }\
-        \
-          #readfree-link > a {\
-            color: white;\
-          }\
-        ";
-        GM_addStyle(custom_css);
+        GM_addStyle(GM_getResourceText("custom_css"));
     }
 
     function dom(tag, attr, inner) {
@@ -60,15 +46,16 @@
     var rf_url = 'http://readfree.me/book/' + id;
 
     GM_xmlhttpRequest({
-        method: "GET",
+        method: "HEAD",
         url: rf_url,
         onload: function(response) {
-            if (response.readyState == 4 && response.status == 200) {
-                add_readfree_style();
+            if (response.status == 200) {
                 var panel = dom('div', { id : 'readfree-link' }),
                     ahref = dom('a', { href : rf_url, target : '_blank' }, 'ReadFree!')
                 panel.appendChild(ahref);
                 document.body.appendChild(panel);
+
+                add_readfree_style();
             }
         }
     });
